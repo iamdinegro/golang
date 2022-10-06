914bc1f81dbd0715fd07034e37efdebac74b734f
iamdinegro <diego.devops@hotmail.com>
2022-10-06 00:07:08 -0300

    Adding new file

--- curso_Go_for_DevOps/helloworld/main.go
+++ curso_Go_for_DevOps/helloworld/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
 	args := os.Args
-	fmt.Printf("Hello, World!\nArguments: %v\n", args[1:len(args)])
+
+	if len(args) < 2 {
+		fmt.Println("Usage: ./http-get <url>\n")
+		os.Exit(1)
+	}
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
+
 }
